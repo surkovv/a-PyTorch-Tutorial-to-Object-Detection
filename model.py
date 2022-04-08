ef23161eb9e1d09eb3ccc4b68272ef9b2f68154e
Vyacheslav Surkov <60709761+surkovv@users.noreply.github.com>
2022-04-08 18:41:49 +0300

    Update model.py

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from math import sqrt
 from itertools import product as product
 import torchvision
+import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -206,83 +207,6 @@ class AuxiliaryConvolutions(nn.Module):
 
         # Higher-level feature maps
         return conv8_2_feats, conv9_2_feats, conv10_2_feats, conv11_2_feats
-    
-class NewAuxiliaryConvolutions(nn.Module):
-    """
-    Additional convolutions to produce higher-level feature maps.
-    """
-
-    def __init__(self):
-        super(NewAuxiliaryConvolutions, self).__init__()
-
-        # Auxiliary/additional convolutions on top of the VGG base
-        self.conv8_1 = nn.Conv2d(1024, 256, kernel_size=1, padding=0)  # stride = 1, by default
-        self.conv8_2 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1)  # dim. reduction because stride > 1
-
-        self.conv9_1 = nn.Conv2d(512, 128, kernel_size=1, padding=0)
-        self.conv9_2 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)  # dim. reduction because stride > 1
-
-        self.conv10_1 = nn.Conv2d(256, 128, kernel_size=1, padding=0)
-        self.conv10_2 = nn.Conv2d(128, 256, kernel_size=3, padding=0)  # dim. reduction because padding = 0
-
-        self.conv11_1 = nn.Conv2d(256, 128, kernel_size=1, padding=0)
-        self.conv11_2 = nn.Conv2d(128, 256, kernel_size=3, padding=0)  # dim. reduction because padding = 0
-        
-        self.conv12_1 = nn.ConvTranspose2d(256, 128, kernel_size=3, padding=0)
-        self.conv12_2 = nn.ConvTranspose2d(128, 256, kernel_size=1, padding=0)
-
-        self.conv13_1 = nn.ConvTranspose2d(256, 128, kernel_size=3, padding=0)
-        self.conv13_2 = nn.ConvTranspose2d(128, 256, kernel_size=1, padding=0)
-
-        # Initialize convolutions' parameters
-        self.init_conv2d()
-
-    def init_conv2d(self):
-        """
-        Initialize convolution parameters.
-        """
-        for c in self.children():
-            if isinstance(c, nn.Conv2d):
-                nn.init.xavier_uniform_(c.weight)
-                nn.init.constant_(c.bias, 0.)
-
-    def forward(self, conv7_feats):
-        """
-        Forward propagation.
-
-        :param conv7_feats: lower-level conv7 feature map, a tensor of dimensions (N, 1024, 19, 19)
-        :return: higher-level feature maps conv8_2, conv9_2, conv10_2, and conv11_2
-        """
-        out = F.relu(self.conv8_1(conv7_feats))  # (N, 256, 19, 19)
-        out = F.relu(self.conv8_2(out))  # (N, 512, 10, 10)
-        conv8_2_feats = out  # (N, 512, 10, 10)
-
-        out = F.relu(self.conv9_1(out))  # (N, 128, 10, 10)
-        out = F.relu(self.conv9_2(out))  # (N, 256, 5, 5)
-        conv9_2_feats = out  # (N, 256, 5, 5)
-
-        out = F.relu(self.conv10_1(out))  # (N, 128, 5, 5)
-        out = F.relu(self.conv10_2(out))  # (N, 256, 3, 3)
-        conv10_2_feats = out  # (N, 256, 3, 3)
-
-        out = F.relu(self.conv11_1(out))  # (N, 128, 3, 3)
-        out = F.relu(self.conv11_2(out))  # (N, 256, 1, 1)
-        conv11_2_feats = out
-        
-        out = F.relu(self.conv12_1(out))  # (N, 128, 3, 3)
-        out = F.relu(self.conv12_2(out))  # (N, 256, 1, 1)
-        conv12_2_feats = out
-        
-        out = F.relu(self.conv13_1(out))  # (N, 128, 3, 3)
-        out = F.relu(self.conv13_2(out))  # (N, 256, 1, 1)
-        conv13_2_feats = out
-        
-        # Higher-level feature maps
-        return conv8_2_feats, conv9_2_feats, conv10_2_feats, conv11_2_feats, conv12_2_feats, conv13_2_feats
-
-
-    
-    
     
 class PredictionConvolutions(nn.Module):
     """
